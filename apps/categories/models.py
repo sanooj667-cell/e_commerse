da086273c3e58bed0ff4ef55ecce7aa5b0a86a4d
Sanooj <sanooj667@gmail.com>
2026-03-11 15:35:09 +0530

    Creat a prodect model

--- apps/categories/models.py
+++ apps/categories/models.py
@@ -13,3 +13,20 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+    
+    
+    
+class Product(models.Model):
+
+    name = models.CharField(max_length=200)
+    description = models.TextField(blank=True, null=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="products")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = "products"
+        ordering = ["-id"]
+
+    def __str__(self):
+        return self.name
