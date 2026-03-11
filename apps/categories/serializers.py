9d139cba09c2d2ee5ab558c726d3d71f2c2f4887
Sanooj <sanooj667@gmail.com>
2026-03-11 15:40:31 +0530

    Creat a ProductSerializer

--- apps/categories/serializers.py
+++ apps/categories/serializers.py
@@ -7,3 +7,10 @@ class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = "__all__"
+
+
+class ProductSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Category
+        fields = "__all__"
