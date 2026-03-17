183c0ab9e328c894f16bb8397e7faca93b243074
Sanooj <sanooj667@gmail.com>
2026-03-17 12:55:29 +0530

    complete the order model section

--- backend/apps/orders/models.py
+++ backend/apps/orders/models.py
@@ -1,4 +1,37 @@
 from django.db import models
+from apps.categories.models import Product
 
 # Create your models here.
+
+class Order(models.Model):
+    user = models.ForeignKey("users.User", on_delete=models.CASCADE, related_name="orders")
+    total_price = models.DecimalField(max_digits=10, decimal_places=2)
+    status = models.CharField(max_length=50, default="pending")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = "orders"
+        ordering = ["-id"]
+
+    def __str__(self):
+        return f"Order {self.id} by {self.user.username}" 
+    
+
+    
+
+class OrderItem(models.Model):
+
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name="items")
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+
+    quantity = models.IntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    class Meta:
+        db_table = "order_items"
+
+    def __str__(self):
+        return f"{self.quantity} x {self.product.name} for Order {self.order.id}"   
+    
+
 
